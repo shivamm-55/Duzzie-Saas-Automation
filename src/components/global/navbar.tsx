7896941551a822ab82c915b67123d92b1c9fdc0d
shivamm-55 <shivammishra76207@gmail.com>
2024-05-16 23:54:20 +0530

    Final Commit

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async (props: Props) => {
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
-        <p className="text-3xl font-bold">Fu</p>
+        <p className="text-3xl font-bold">Du</p>
         <Image
           src="/fuzzieLogo.png"
           width={15}
